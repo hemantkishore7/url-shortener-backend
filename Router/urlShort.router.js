8b7d17a3f85c3ebeff49051d5d72e9116a620f4c
hemantkishore7 <hemantkishore98@gmail.com>
2023-06-13 20:10:53 +0530

    update

--- Router/urlShort.router.js
+++ Router/urlShort.router.js
@@ -1,89 +1,75 @@
 const urlShortRouter = require("express").Router();
 const shortid = require("shortid");
-const Url = require("../Model/Url.model")
+const Url = require("../Model/Url.model");
 
+//Get all URL
+urlShortRouter.get("/getAll", async (req, res) => {
+  let url;
+  try {
+    url = await Url.find();
+  } catch (error) {
+    console.log(error);
+  }
+  if (!url) {
+    return res.status(400).json("No more URL");
+  }
+  res.status(200).json({
+    message: "fetched all URL",
+    data: url,
+  });
+});
 
 //generate short Url
-urlShortRouter.post("/generate",async(req,res)=>{
-    const {longUrl} = req.body;
-    if(!longUrl) return res.status(404).send({error:"URL is required"})
+urlShortRouter.post("/generate", async (req, res) => {
+  const { longUrl } = req.body;
+  if (!longUrl) return res.status(404).send({ error: "URL is required" });
 
-    const shortID = shortid();
+  const shortID = shortid();
 
-   const data = await Url.create({
-        longUrl:longUrl,
-        shortUrl:shortID,
-        redirectURL:longUrl,
-        clickCount:0
-    })
+  const data = await Url.create({
+    longUrl: longUrl,
+    shortUrl: shortID,
+    redirectURL: longUrl,
+    clickCount: 0,
+  });
 
-    return res.status(200).json({data:data})
-})
+  return res.status(200).json({ data: data });
+});
 
 //Count how many times click the link
-urlShortRouter.get("/:id", async(req,res)=>{
-   try {
-    await Url.findOne({shortUrl:req.params.id},(err,data)=>{
-        if(err) {
-            return res.status(400).send('Error while redirect url')
-        }
-        Url.findByIdAndUpdate({_id:data.id},{$inc : {clickCount:1}},(err,data)=>{
-            if(err){
-                res.status(400).send('Error while redirect url')
-            }
-            res.redirect(data.longUrl)
-        })
-    })
-   } catch (error) {
-    return res.status(500).json({
-        message:'Internal Server Error',
-         error:error})
-   }
-})
-
-//Get all url
-urlShortRouter.get("/getAll",async function(req,res){
-    let data;
-    try {
-       const data = await Url.find();
-    }
-    catch(error) {
-        console.log(error);
-        // res.status(500).json({
-        //     message:'Internal Server Error',
-        //      error:error
-        //     })
-
-    };
-    if(!data) return res.status(400).json("unexcepted error")
-    res.status(200).json({
-     message:"fetched all the URL",
-     data:data
-    })
-})
+urlShortRouter.get("/:id", async (req, res) => {
+  let url;
+  try {
+    url = await Url.findOne({ _id: req.params.id });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 //Get Url by id
-urlShortRouter.get("/get/:id",async(req,res)=>{
+urlShortRouter.get("/get/:id", async (req, res) => {
+  let data;
     try {
-        const id = req.params.id;
-        const data = await Url.findOne({_id:id})
-        if(!data) return res.status(400).send("Cannot able to fetch URL")
-        return res.status(200).send({success:true,data:data})
-    } catch (error) {
-        res.status(500).send('Internal Server Error', err)
-    }
-})
+    const id = req.params.id;
+     data = await Url.findOne({ _id: id });
+     data = await Url.updateOne({_id:id},{$inc :{clickCount:1}})
+  } catch (error) {
+    console.log(error)
+  }
+  if (!data) return res.status(400).send("Cannot able to fetch URL");
+  return res.status(200).send({ success: true, data: data });
+});
 
 //Delete url
-urlShortRouter.delete("/delete/:id",async(req,res)=>{
-    try {
-       const id = req.params.id;
-       const data = await Url.deleteOne({_id:id})
-       if(!data) return res.status(400).send("cannot delete the url")
-        return res.status(200).send({success:true,message:"URL deleted"})
-    } catch (error) {
-        res.status(500).send('Internal Server Error', err)
-    }
-})
+urlShortRouter.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Url.deleteOne({ _id: id });
+    if (!data) return res.status(400).send("cannot delete the url");
+    return res.status(200).send({ success: true, message: "URL deleted" });
+  } catch (error) {
+    res.status(500).send("Internal Server Error", err);
+  }
+});
 
-module.exports = urlShortRouter
+module.exports = urlShortRouter;
